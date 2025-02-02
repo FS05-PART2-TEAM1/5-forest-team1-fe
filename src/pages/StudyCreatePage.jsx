d29af1b1d69bbf3df2f25e45b967827a58eea08a
silverraining <www.stella.choi@gmail.com>
2025-02-02 11:32:29 -0800

    feat: cross env 설정

--- src/pages/StudyCreatePage.jsx
+++ src/pages/StudyCreatePage.jsx
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
+
 import btn_visible_on from "../assets/icons/btn_visibility_on.svg";
 import btn_visible_off from "../assets/icons/btn_visibility_off.svg";
+import bg1 from "../assets/mock_bg_images/bg_color_orange.jpg";
+import bg2 from "../assets/mock_bg_images/bg_color_pink.jpg";
+import bg3 from "../assets/mock_bg_images/bg_color_skyblue.jpg";
+import bg4 from "../assets/mock_bg_images/bg_yellowpattern.jpg";
+import bg5 from "../assets/mock_bg_images/bg_flight.jpg";
+import bg6 from "../assets/mock_bg_images/bg_flower.jpg";
+import bg7 from "../assets/mock_bg_images/bg_sea.jpg";
+import bg8 from "../assets/mock_bg_images/bg_loveblue.jpg";
+
+const images = [bg1, bg2, bg3, bg4, bg5, bg6, bg7, bg8];
+
 function StudyCreatePage() {
   const [isVisible, setIsVisible] = useState(false);
 
@@ -12,11 +24,11 @@ function StudyCreatePage() {
       <div className=" rounded-xl lg:mt-[27px] lg:mb-32 md:mb-[197px] mt-5 mb-[171px] lg:w-[696px] lg:h-[1163px] md:w-[696px] md:h-[1171px] w-[344px] h-[1423px]  bg-white  p-4">
         <div className="mt-2">
           <div>
-            <div className="gap-6">
+            <div className="">
               <div className="mb-6">
                 <h1 className="text-2xl font-bold">스터디 만들기</h1>
               </div>
-              <form className="flex flex-col mb-4 gap-2">
+              <form className="flex flex-col mb-6 gap-2">
                 <label className="text-lg font-semibold" htmlFor="">
                   닉네임
                 </label>
@@ -25,7 +37,7 @@ function StudyCreatePage() {
                   placeholder="닉네임을 입력해 주세요"
                 ></input>
               </form>
-              <form className="flex flex-col mb-4 gap-2">
+              <form className="flex flex-col mb-6 gap-2">
                 <label className="text-lg font-semibold" htmlFor="">
                   스터디 이름
                 </label>
@@ -34,7 +46,7 @@ function StudyCreatePage() {
                   placeholder="스터디 이름을 입력해주세요"
                 ></input>
               </form>
-              <form className="flex flex-col mb-4 gap-2">
+              <form className="flex flex-col mb-6 gap-4">
                 <label className="text-lg font-semibold" htmlFor="">
                   소개
                 </label>
@@ -43,12 +55,27 @@ function StudyCreatePage() {
                   placeholder="소개 멘트를 작성해 주세요"
                 />
               </form>
-              <div className=" mb-4 gap-2">
-                <h3 className="text-lg font-semibold">배경을 선택해주세요</h3>
-                <div>이미지들 </div>
+              <div className=" mb-6 ">
+                <h3 className="text-lg font-semibold mb-6">
+                  배경을 선택해주세요
+                </h3>
+                <div className="grid grid-cols-2  md:grid-cols-4 lg:grid-cols-4 gap-4">
+                  {images.map((image, index) => (
+                    <div
+                      key={index}
+                      className="w-[150px] h-[150px] rounded-xl overflow-hidden"
+                    >
+                      <img
+                        src={image}
+                        alt={`Image ${index + 1}`}
+                        className="w-full h-full object-cover"
+                      />
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
-            <form className="flex flex-col mb-4 gap-2">
+            <form className="flex flex-col mb-6 gap-2">
               <label className="text-lg font-semibold" htmlFor="">
                 비밀번호
               </label>
