aaa336078dfd33f1b716207c3af47c352c69ca19
Ripponcheese <hayooncode@gmail.com>
2025-02-03 00:15:34 +0900

    feat: 공통컴포넌트-에러메세지

---
+++ src/common/MessageBox.jsx
@@ -0,0 +1,10 @@
+const ErrorMessage = ({ message }) => {
+  return (
+    <div className="w-fit bg-[#FDE0E9] text-[#F50E0E] px-7 py-3 rounded-2xl flex items-center gap-2 font-medium text-[16px]">
+      <span>🚨</span>
+      <span>{message}</span>
+    </div>
+  );
+};
+
+export default ErrorMessage;
