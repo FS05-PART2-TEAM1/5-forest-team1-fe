5a1db0ed2c5bfefab7410f078e9975f6620e084b
silverraining <www.stella.choi@gmail.com>
2025-02-03 03:47:11 -0800

    Feat: 이모지태그 코드 간소화 및 사이즈 분리

--- src/common/EmojiTag.jsx
+++ src/common/EmojiTag.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 
-const EmojiTag = ({ emojis }) => {
+const EmojiTag = ({ emoji, count, size = "sm" }) => {
+  //부모컴포넌트에서 렌더링시킬때 이모지태그 사이즈 조정 ex. <EmojiTag size="md" />
+
+  const emojiSizeClasses = {
+    sm: "text-16pt",
+    md: "text-[18px]",
+  };
+
+  const textSizeClasses = {
+    sm: "text-[12px]",
+    md: "text-[16px]",
+  };
+
   return (
-    <div className="relative">
-      <div className=" inline-flex h-6  rounded-full items-center gap-2 ">
-        {emojis.map((e, index) => (
-          <div
-            key={index}
-            className="flex items-center gap-1 bg-f-gray-500 px-2 py-1 rounded-full"
-          >
-            <span>{e.emoji}</span>
-            <span className="text-sm text-f-black">{e.count}</span>
-          </div>
-        ))}
-      </div>
+    <div className="flex items-center gap-1 px-2 py-1 bg-f-gray-500 bg-opacity-40 rounded-full">
+      <span className={`${emojiSizeClasses[size]}`}>{emoji}</span>
+      <span className={`${textSizeClasses[size]} text-white`}>{count}</span>
     </div>
   );
 };
