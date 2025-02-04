395303a83cb6262d84a2b77100f499e9e7a67e0b
silverraining <www.stella.choi@gmail.com>
2025-02-03 18:24:58 -0800

    design: tailwind css 반응형 사이즈 적용

--- src/common/EmojiTag.jsx
+++ src/common/EmojiTag.jsx
@@ -1,22 +1,10 @@
 import React from "react";
 
-const EmojiTag = ({ emoji, count, size = "sm" }) => {
-  //부모컴포넌트에서 렌더링시킬때 이모지태그 사이즈 조정 ex. <EmojiTag size="md" />
-
-  const emojiSizeClasses = {
-    sm: "text-16pt",
-    md: "text-[18px]",
-  };
-
-  const textSizeClasses = {
-    sm: "text-[12px]",
-    md: "text-[16px]",
-  };
-
+const EmojiTag = ({ emoji, count }) => {
   return (
-    <div className="flex items-center gap-1 px-2 py-1 bg-f-gray-500 bg-opacity-40 rounded-full">
-      <span className={`${emojiSizeClasses[size]}`}>{emoji}</span>
-      <span className={`${textSizeClasses[size]} text-white`}>{count}</span>
+    <div className="flex items-center gap-1 px-2 py-1 bg-f-black bg-opacity-40 rounded-full md:text-sm lg:text-lg">
+      <span>{emoji}</span>
+      <span className="text-white">{count}</span>
     </div>
   );
 };
