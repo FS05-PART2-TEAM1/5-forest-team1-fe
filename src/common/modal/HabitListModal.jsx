3359f96779501135f7c96c6ab6d3f595bbccd6a4
Syyga <gsh2672@gmail.com>
2025-02-04 15:34:19 +0900

    fix: 모달 기능 프롭으로 받도록 수정

--- src/common/modal/HabitListModal.jsx
+++ src/common/modal/HabitListModal.jsx
@@ -1,20 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import deleteHabitImg from "./img/deleteHabit.png";
 import addHabitImg from "./img/addHabit.png";
 
-const HabitListModal = ({ isOpen, onClose, onSave }) => {
-  const [habits, setHabits] = useState([]);
-
+const HabitListModal = ({
+  isOpen,
+  onClose,
+  onSave,
+  habits = [],
+  onAddHabit,
+  onRemoveHabit,
+}) => {
   if (!isOpen) return null;
-
-  const addHabit = () => {
-    const newHabit = prompt("새로운 습관을 입력하세요.");
-    if (newHabit) setHabits([...habits, newHabit]);
-  };
-
-  const removeHabit = (index) => {
-    setHabits(habits.filter((_, i) => i !== index));
-  };
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50 p-4">
@@ -25,37 +21,48 @@ const HabitListModal = ({ isOpen, onClose, onSave }) => {
 
         <div className="flex flex-col gap-[20px] px-[100px] mt-6 overflow-y-auto max-h-[60vh]">
           <ul className="relative flex flex-col gap-[20px]">
-            {habits.map((habit, index) => (
-              <li
-                key={index}
-                className="relative flex items-center justify-center bg-[#EEEEEE] rounded-[20px] h-[54px]"
-              >
-                <span className="text-[16px] text-[#818181] underline">
-                  {habit}
-                </span>
-
-                <button
-                  onClick={() => removeHabit(index)}
-                  className="absolute -right-[56px] top-1/2 transform -translate-y-1/2 w-12 h-12 flex items-center justify-center bg-[#FDE0E9] rounded-full"
+            {habits.length > 0 ? (
+              habits.map((habit, index) => (
+                <li
+                  key={index}
+                  className="relative flex items-center justify-center bg-[#EEEEEE] rounded-[20px] h-[54px]"
                 >
-                  <img src={deleteHabitImg} alt="삭제" className="w-6 h-6" />
-                </button>
-              </li>
-            ))}
+                  <span className="text-[16px] text-[#818181] underline">
+                    {habit}
+                  </span>
+                  {onRemoveHabit && (
+                    <button
+                      onClick={() => onRemoveHabit(index)}
+                      className="absolute -right-[56px] top-1/2 transform -translate-y-1/2 w-12 h-12 flex items-center justify-center bg-[#FDE0E9] rounded-full"
+                    >
+                      <img
+                        src={deleteHabitImg}
+                        alt="삭제"
+                        className="w-6 h-6"
+                      />
+                    </button>
+                  )}
+                </li>
+              ))
+            ) : (
+              <li className="text-center text-[#818181]"></li>
+            )}
           </ul>
 
-          <button
-            onClick={addHabit}
-            className="w-full flex justify-center items-center border-2 border-black rounded-[20px] h-[54px] p-0"
-          >
-            <img src={addHabitImg} alt="추가" className="w-6 h-6" />
-          </button>
+          {onAddHabit && (
+            <button
+              onClick={onAddHabit}
+              className="w-full flex justify-center items-center border-2 border-black rounded-[20px] h-[54px] p-0"
+            >
+              <img src={addHabitImg} alt="추가" className="w-6 h-6" />
+            </button>
+          )}
         </div>
 
         <div className="flex justify-between gap-6 mt-6">
           <button
             onClick={onClose}
-            className="px-5 py-3 bg-[#DDDDDD] text-white  rounded-lg w-1/2"
+            className="px-5 py-3 bg-[#DDDDDD] text-white rounded-lg w-1/2"
           >
             닫기
           </button>
