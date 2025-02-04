f1a4b6120c01eba46c10b4157300b9331b164c37
Ripponcheese <hayooncode@gmail.com>
2025-02-05 00:25:19 +0900

    feat: 집중 성공시 이벤트 추가

--- src/common/MessageBox.jsx
+++ src/common/MessageBox.jsx
@@ -1,7 +1,13 @@
-const ErrorMessage = ({ message }) => {
+const ErrorMessage = ({ message, isCompleted }) => {
   return (
-    <div className="w-fit bg-[#FDE0E9] text-[#F50E0E] px-7 py-3  rounded-2xl flex items-center gap-2 font-medium text-[14px] md:text-[16px] mx-auto">
-      <span>🚨</span>
+    <div
+      className={`w-fit ${
+        isCompleted
+          ? "bg-[#E1EDDE] text-[#578246]"
+          : "bg-[#FDE0E9] text-[#F50E0E]"
+      } px-7 py-3 rounded-2xl flex items-center gap-2 font-medium text-[14px] md:text-[16px] mx-auto`}
+    >
+      <span>{isCompleted ? "🎉" : "🚨"}</span>
       <span>{message}</span>
     </div>
   );
