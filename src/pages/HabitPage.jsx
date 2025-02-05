0bc1f6cca50549c341c9eb86abadec1f78aa3809
Syyga <gsh2672@gmail.com>
2025-02-05 10:35:49 +0900

    fix: 반응형 스타일 수정

--- src/pages/HabitPage.jsx
+++ src/pages/HabitPage.jsx
@@ -20,7 +20,7 @@ function HabitPage() {
 
       {/* Main Content */}
       <main className="p-[20px] sm:p-[16px_24px] md:p-[16px_24px] ">
-        <div className="bg-white rounded-lg shadow p-6 min-[1200px]:w-[1248px] mx-auto">
+        <div className="bg-white rounded-lg shadow p-6 min-[1200px]:w-[1150px] mx-auto">
           <h2 className="text-2xl font-semibold mb-4">스터디 컨테이너</h2>
           <p>여기에 스터디 관련 내용</p>
           {/* 추가적인 스터디 관련 컴포넌트나 UI 요소를 이곳에 추가하세요 */}
