cc3b886507ba7a6d6a38d0bd42a663f40f86ab60
Syyga <gsh2672@gmail.com>
2025-02-05 10:21:35 +0900

    fix:테스트 페이지 삭제

--- src/pages/HabitPage.jsx
+++ src/pages/HabitPage.jsx
@@ -1,7 +1,7 @@
 function HabitPage() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">오늘의 습관 페이지 - 가승연</h1>{" "}
+      <h1 className="text-3xl font-bold">오늘의 습관 페이지 - 가승연</h1>
     </div>
   );
 }
