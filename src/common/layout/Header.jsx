cdfe681f82d553fe419d5197a4f154409abd113b
hyuk-dev <hyuk.development@gmail.com>
2025-02-05 09:27:37 +0900

    feat: 버튼 적용 및 로고와 버튼에 링크 적용

--- src/common/layout/Header.jsx
+++ src/common/layout/Header.jsx
@@ -1,18 +1,28 @@
+import { Link } from "react-router-dom";
 import logoImg from "../../assets/img_logo.png";
+import HeaderButton from "../buttons/HeaderButton";
 
 export function Header({ isCreateButton }) {
   return (
     <>
       {isCreateButton ? (
         <div className="flex lg:ml-48 md:ml-8 ml-2 lg:mr-48 md:mr-8 mr-2 mt-4 lg:mb-8 mb-4 justify-between items-center">
-          <img className="md:w-[182px] w-[106px] md:h-[60px] h-[35px]" src={logoImg} />
-          {/* 버튼 넣어야합니다. */}
-          <div className="hover:bg-green-300 bg-orange-300 flex justify-center items-center p-4 text-white rounded-full cursor-pointer">나는 가짜버튼</div>
+          <Link to="/">
+          <img
+            className="md:w-[182px] w-[106px] md:h-[60px] h-[35px]"
+            src={logoImg}
+          />
+          </Link>
+          <Link to="/study/create">
+            <HeaderButton>스터디 만들기</HeaderButton>
+          </Link>
         </div>
       ) : (
+        <Link to="/">
         <div className="ml-48 mt-4 mb-8">
           <img src={logoImg} />
         </div>
+        </Link>
       )}
     </>
   );
