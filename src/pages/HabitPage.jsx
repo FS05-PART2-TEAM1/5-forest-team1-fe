9905144dd121db422d7284ba3e0f5901a5b5431d
Syyga <gsh2672@gmail.com>
2025-02-05 10:19:00 +0900

    fix: 반응형 스타일 수정

--- src/pages/HabitPage.jsx
+++ src/pages/HabitPage.jsx
@@ -1,28 +1,28 @@
 import React from "react";
+import HeaderImage from "../assets/icons/img_logo.png";
 
 function HabitPage() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-[#F6F4EF]">
       {/* Header */}
-      <header className="flex items-center p-4 bg-white shadow">
+      <header className="flex items-center bg-[#F6F4EF]  h-[100px] bg">
         <img
           src={HeaderImage}
           alt="헤더 이미지"
-          className="w-12 h-12 mr-4 object-cover rounded-full"
+          className="
+            max-[763px]:w-[102px] 
+            max-[763px]:pl-[16px] max-[763px]:pt-[32px] max-[763px]:pb-[32px]
+            min-[764px]:pl-[24px]
+            min-[1200px]:pl-[220px] min-[1200px]:pt-[20px] min-[1200px]:pb-[20px]
+          "
         />
-        <h1 className="text-3xl font-bold text-gray-800">
-          오늘의 습관 페이지 - 가승연
-        </h1>
       </header>
 
       {/* Main Content */}
-      <main className="p-4">
-        <div className="bg-white rounded-lg shadow p-6">
+      <main className="p-[20px] sm:p-[16px_24px] md:p-[16px_24px] ">
+        <div className="bg-white rounded-lg shadow p-6 min-[1200px]:w-[1248px] mx-auto">
           <h2 className="text-2xl font-semibold mb-4">스터디 컨테이너</h2>
-          <p>
-            여기에 스터디 관련 내용이 표시됩니다. 예를 들어, 스터디 목록, 스터디
-            생성/수정 등의 기능을 포함할 수 있습니다.
-          </p>
+          <p>여기에 스터디 관련 내용</p>
           {/* 추가적인 스터디 관련 컴포넌트나 UI 요소를 이곳에 추가하세요 */}
         </div>
       </main>
