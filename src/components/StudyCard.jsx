f07879236805ab8b41723881ee36bc83a2d62fac
seha <hansaemj20@gmail.com>
2025-02-06 11:16:46 +0900

    임시저장

--- src/components/StudyCard.jsx
+++ src/components/StudyCard.jsx
@@ -11,6 +11,12 @@ const backgroundColors = {
   blue: "bg-f-blue-blue",
   pink: "bg-f-pink-bg",
 };
+const textColors = {
+  green: "text-f-green-text",
+  yellow: "text-f-yellow-text",
+  blue: "text-f-blue-text",
+  pink: "text-f-pink-text",
+};
 
 const StudyCard = ({ study, type = "browse" }) => {
   const isImage = study.backgroundType === "image";
@@ -34,7 +40,7 @@ const StudyCard = ({ study, type = "browse" }) => {
   // 텍스트 색상 설정 (이미지 배경이면 흰색, 아니면 지정된 색상)
   const nameTextColor = isImage
     ? "text-[#ffffff]"
-    : `text-f-${study.backgroundContent}-text`;
+    : `${textColors[study.backgroundContent]}`;
   const titleTextColor = isImage ? "text-[#ffffff]" : "text-f-black";
   const infoTextColor = isImage ? "text-f-gray-100" : "text-f-black";
   const descriptionTextColor = isImage ? "text-[#ffffff]" : "text-f-black";
@@ -74,7 +80,7 @@ const StudyCard = ({ study, type = "browse" }) => {
       </span>
 
       <span
-        className={`text-[14px] ${descriptionTextColor} md:text-16pt font-normal max-w-[208px] md:max-w-none leading-tight md:leading-snug lg:leading-normal block`}
+        className={`text-[14px] ${descriptionTextColor} md:text-16pt font-normal max-w-[208px] md:max-w-none leading-tight md:leading-snug lg:leading-normal line-clamp-2`}
       >
         {study.description}
       </span>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,72 @@
+import StudyCard from "@/components/StudyCard";
+
+const STUDY = [
+  {
+    id: "70c8c2ef-dca8-4667-924c-140cd339712b",
+    nickname: "새하",
+    title: "웹 보안 스터디",
+    description:
+      "웹 보안과 관련된 개념을 학습합니다.웹 보안과 관련된 개념을웹 보안과 관련된 개념을웹 보안과 관련된 개념을 ",
+    backgroundType: "image",
+    backgroundContent: `https://picsum.photos/200/200`,
+    points: 180,
+    createdAt: "2024-03-30T00:00:00.000Z",
+    updatedAt: "2025-01-30T08:42:58.828Z",
+    reactions: [
+      {
+        id: "dc77b00b-795d-4490-8679-8eff574027be",
+        studyId: "cf38c6d9-686a-44de-8490-2a2ffa1bcba1",
+        emoji: "👍",
+        counts: 5,
+        createdAt: "2025-01-31T15:22:05.752Z",
+        updatedAt: "2025-01-31T15:22:05.752Z",
+      },
+      {
+        id: "7d702831-04e5-4937-910c-029adb385eb5",
+        studyId: "cf38c6d9-686a-44de-8490-2a2ffa1bcba1",
+        emoji: "🔥",
+        counts: 3,
+        createdAt: "2025-01-31T15:22:05.754Z",
+        updatedAt: "2025-01-31T15:22:05.754Z",
+      },
+    ],
+  },
+  {
+    id: "70c8c2ef-dca8-4667-924c-140cd339712b",
+    nickname: "새하",
+    title: "웹 보안 스터디",
+    description: "웹 보안과 관련된 개념을 학습합니다.",
+    backgroundType: "color",
+    backgroundContent: "green",
+    points: 180,
+    createdAt: "2024-03-30T00:00:00.000Z",
+    updatedAt: "2025-01-30T08:42:58.828Z",
+    reactions: [],
+  },
+];
 function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold">스터디 목록 - 정한샘</h1>
+      <div className="max-w-[1200px] px-[38px]">
+        최근 조회한 스터디
+        <div className="flex gap-[16px] md:gap-[24px] overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          <StudyCard study={STUDY[0]} type={"viewed"}></StudyCard>
+          <StudyCard study={STUDY[0]} type={"viewed"}></StudyCard>
+          <StudyCard study={STUDY[0]} type={"viewed"}></StudyCard>
+        </div>
+      </div>
+      <div className="max-w-[1200px] px-[38px]">
+        스터디 둘러보기
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-y-[16px] md:gap-[24px]">
+          <StudyCard study={STUDY[0]}></StudyCard>
+          <StudyCard study={STUDY[0]}></StudyCard>
+          <StudyCard study={STUDY[1]}></StudyCard>
+          <StudyCard study={STUDY[1]}></StudyCard>
+          <StudyCard study={STUDY[0]}></StudyCard>
+          <StudyCard study={STUDY[0]}></StudyCard>
+        </div>
+      </div>
     </div>
   );
 }
