0788cf7539d0113fac56ed8c9d39b4a74e3e1bc7
hyuk-dev <hyuk.development@gmail.com>
2025-02-08 20:19:31 +0900

    fix: 이모지 폼 UX 개선

--- src/components/EmojiForm.jsx
+++ src/components/EmojiForm.jsx
@@ -6,7 +6,6 @@ import { useEffect, useState } from "react";
 import { getReactions, patchReaction, postReaction } from "@/api/reactionApi";
 
 function EmojiForm({ studyId }) {
-
   const [emojis, setEmojis] = useState([]);
   const [isAddMod, setIsAddMod] = useState(false);
   const [isShowAll, setIsShowAll] = useState(false);
@@ -30,12 +29,16 @@ function EmojiForm({ studyId }) {
 
   const onEmojiTagClick = async (emoji) => {
     const findEmoji = emojis.find((element) => element.emoji === emoji);
-    setEmojis(emojis.map((element)=> {
-      if(element.emoji === emoji) return {...element, counts: element.counts+1}
-      else return element;
-    }))
-    await patchReaction(studyId, findEmoji.id, {counts: 1})
-  }
+    setEmojis(
+      emojis.map((element) => {
+        if (element.emoji === emoji)
+          return { ...element, counts: element.counts + 1 };
+        else return element;
+      })
+    );
+    await patchReaction(studyId, findEmoji.id, { counts: 1 });
+    setIsChanged(true);
+  };
 
   const onEmojiClick = async (emojiData) => {
     const isEmoji = emojis.find((element) => emojiData.emoji === element.emoji);
@@ -58,30 +61,53 @@ function EmojiForm({ studyId }) {
     setIsAddMod(false);
   };
 
+  useEffect(() => {
+    if(isChanged){
+      const sortedEmojis = [...emojis].sort((a, b) => b.counts - a.counts);
+      setEmojis(sortedEmojis);
+      setIsChanged(false)
+    }
+  }, [isChanged]);
+
   return (
     <div className="flex gap-4">
       <div className="flex gap-1">
         {emojis.map((element, index) => {
           if (index < 3)
-            return <div className="cursor-pointer" key={index} onClick={() => onEmojiTagClick(element.emoji)}><EmojiTag emoji={element.emoji} count={element.counts} /></div>;
+            return (
+              <div
+                className="cursor-pointer"
+                key={index}
+                onClick={() => onEmojiTagClick(element.emoji)}
+              >
+                <EmojiTag emoji={element.emoji} count={element.counts} />
+              </div>
+            );
         })}
       </div>
       <div className="flex">
         {emojis.length > 3 && (
           <div className="lg:block hidden">
-          <div
-            className="cursor-pointer flex gap-1 p-2 rounded-[50px] items-center text-[14px] text-white bg-black opacity-30"
-            onClick={onShowAllClick}
-          >
-            <img src={plusImg} />
-            {emojis.length - 3}..
-          </div>
+            <div
+              className="cursor-pointer flex gap-1 p-2 rounded-[50px] items-center text-[14px] text-white bg-black opacity-30"
+              onClick={onShowAllClick}
+            >
+              <img src={plusImg} />
+              {emojis.length - 3}..
+            </div>
           </div>
         )}
         {isShowAll && (
-          <div className="lg:visible invisible pl-5 w-[242px] -translate-x-48 border p-4 gap-1 mt-10 absolute bg-white flex flex-wrap rounded-[20px]">
+          <div className="lg:visible invisible pl-5 w-[250px] -translate-x-48 border p-4 gap-1 mt-12 absolute bg-white grid grid-cols-3 place-items-center rounded-[20px] ">
             {emojis.map((element, index) => {
-              return <div className="h-10 flex items-center cursor-pointer"  onClick={() => onEmojiTagClick(element.emoji)}><EmojiTag emoji={element.emoji} count={element.counts} /></div>;
+              return (
+                <div
+                  className="h-10 flex items-center cursor-pointer "
+                  onClick={() => onEmojiTagClick(element.emoji)}
+                >
+                  <EmojiTag emoji={element.emoji} count={element.counts} />
+                </div>
+              );
             })}
           </div>
         )}
