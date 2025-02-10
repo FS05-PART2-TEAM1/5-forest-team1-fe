1189d82c66ad8d1ebc351c3b982b3198e07ddd21
Ripponcheese <hayooncode@gmail.com>
2025-02-10 14:49:12 +0900

    feat: router 추가

--- src/components/FocusHeader.jsx
+++ src/components/FocusHeader.jsx
@@ -1,20 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import bracketIcon from "@/assets/icons/ic_bracket.png";
-
 export const FocusHeader = ({ nickname = "연우", title = "개발공장" }) => {
   return (
-    <div className="flex flex-col items-start justify-between mb-[21px] md:flex-row md:items-center">
+    <div className="flex flex-col items-start justify-between mb-[16px] md:flex-row md:items-center">
       <h1 className="text-[24px] md:text-[32px] font-extrabold text-[#414141] mb-4 md:mb-0">
         {nickname}의 {title}
       </h1>
       <div className="flex gap-4">
-        <button className="px-4 md:px-6 py-2 rounded-[15px] border border-[#dddddd] text-sm md:text-base font-medium text-[#818181]">
-          오늘의 습관{" "}
-          <img src={bracketIcon} alt="" className="inline-block ml-[14px]" />
-        </button>
-        <button className="px-4 md:px-6 py-2 rounded-[15px] border border-[#dddddd] text-sm md:text-base font-medium text-[#818181]">
-          홈 <img src={bracketIcon} alt="" className="inline-block ml-[14px]" />
-        </button>
+        <Link to="/habit">
+          <button className="px-4 md:px-6 py-2 rounded-[15px] border border-[#dddddd] text-sm md:text-base font-medium text-[#818181]">
+            오늘의 습관{" "}
+            <img src={bracketIcon} alt="" className="inline-block ml-[14px]" />
+          </button>
+        </Link>
+        <Link to="/">
+          <button className="px-4 md:px-6 py-2 rounded-[15px] border border-[#dddddd] text-sm md:text-base font-medium text-[#818181]">
+            홈{" "}
+            <img src={bracketIcon} alt="" className="inline-block ml-[14px]" />
+          </button>
+        </Link>
       </div>
     </div>
   );
