d6c0697e858f033eff4a81dfc4738e085d03fbc4
silverraining <www.stella.choi@gmail.com>
2025-02-10 04:10:14 -0800

    design: 반응형 레이아웃 설정 및 가로 세로 스크롤 적용

--- src/components/HabitTracker.jsx
+++ src/components/HabitTracker.jsx
@@ -8,16 +8,18 @@ import Paw5 from "../assets/icons/paws/paw05.png";
 import Paw6 from "../assets/icons/paws/paw06.png";
 import Paw7 from "../assets/icons/paws/paw07.png";
 import Paw8 from "../assets/icons/paws/paw08.png";
-
+import useWheelScroll from "@/hooks/useWheelScroll";
 import habitApi from "../api/habitApi";
 
 function HabitTracker() {
   const [habitList, setHabitList] = useState([]);
   const days = ["월", "화", "수", "목", "금", "토", "일"];
+  const { scrollRef } = useWheelScroll();
 
   const getDayIndex = (dateString) => {
     const date = new Date(dateString);
     date.setUTCHours(date.getUTCHours() + 9); // UTC+9 한국 시간으로 변환
+    const now = new Date();
 
     const dayIndex = date.getDay(); // 0 (일요일) ~ 6 (토요일)
     return dayIndex === 0 ? 6 : dayIndex - 1; // 월요일(0) ~ 일요일(6)로 맞춤
@@ -26,9 +28,10 @@ function HabitTracker() {
     const fetchHabits = async () => {
       try {
         const studyId = "eb119bc0-57d9-4c6c-ad2b-1c3c05a7d12f";
-        const start = "2025-02-02";
-        const end = "2025-02-09";
+        const start = "2025-02-04";
+        const end = "2025-02-10";
         const data = await habitApi.getHabits(studyId, start, end);
+        console.log("Raw Data from API:", data); // habitList 전체 확인
         setHabitList(data.habitList); // 여기서 데이터 설정!
         console.log(data.habitList);
       } catch (error) {
@@ -54,12 +57,15 @@ function HabitTracker() {
 
   return (
     <div className="flex justify-center items-center mt-5 mb-[171px]">
-      <div className="rounded-xl border lg:mt-[27px] lg:mb-32 lg:max-w-[1120px] lg:h-[511px] w-full min-w-[312px] min-h-[450px] bg-white p-4 overflow-x-auto">
-        <h1 className="text-2xl font-bold mb-6">습관 기록표</h1>
+      <div className="rounded-xl border lg:mt-[27px] lg:mb-32 lg:max-w-[1120px] lg:h-[511px] md:max-w-[648px] h-[450px] max-w-[312px]  bg-white p-4 transition-all duration-300">
+        <h1 className="md:text-2xl text-lg font-bold ">습관 기록표</h1>
 
-        <section className="p-4 mt-10">
+        <div
+          ref={scrollRef}
+          className="mt-2 overflow-x-auto overflow-y-scroll max-h-[380px] md:max-h-[400px]"
+        >
           {/* 요일 헤더 */}
-          <div className="grid grid-cols-9 gap-2 md:gap-4 items-center text-[12px] sm:text-[14px] md:text-[18px] mb-4">
+          <div className="min-w-[648px] grid grid-cols-9 gap-2 md:gap-4 items-center sm:text-[14px] md:text-[18px] mb-4">
             <div className="col-span-2"></div>
             {days.map((day) => (
               <div
@@ -75,15 +81,15 @@ function HabitTracker() {
           {habitList.map((habit, habitIndex) => (
             <div
               key={habit.id}
-              className="grid grid-cols-9 gap-2 md:gap-4 items-center mb-4"
+              className="grid grid-cols-9 gap-2 md:gap-4 items-center mb-4 min-w-[648px]"
             >
               {/* 습관 이름 */}
-              <div className="col-span-2 font-medium text-[12px] sm:text-[14px] md:text-[18px] text-right pr-2 sm:pr-4 whitespace-normal max-w-[200px]">
+              <div className="col-span-2 font-medium text-[14px] md:text-[18px] text-right  whitespace-normal max-w-[110px] md:max-w-[200px]">
                 {habit.name}
               </div>
 
               {/* paw 이미지 */}
-              <div className="col-span-7 grid grid-cols-7 gap-1 sm:gap-2 md:gap-4">
+              <div className="col-span-7 grid grid-cols-7 gap-4">
                 {days.map((_, dayIndex) => {
                   const habitStatus = habit.dailyHabitCheck.find(
                     (check) => getDayIndex(check.date) === dayIndex
@@ -97,14 +103,14 @@ function HabitTracker() {
                       key={dayIndex}
                       src={habitStatus && habitStatus.status ? pawImage : paw} // status가 true면 컬러 paw, false면 기본 paw
                       alt="paw"
-                      className="w-5 h-5 sm:w-6 sm:h-6 md:w-8 md:h-8 mx-auto"
+                      className="w-9 h-9 mx-auto my-2"
                     />
                   );
                 })}
               </div>
             </div>
           ))}
-        </section>
+        </div>
       </div>
     </div>
   );
