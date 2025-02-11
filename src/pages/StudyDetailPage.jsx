bcb734be16e38acffe0559ea0c34eda1ad53d536
hyuk-dev <hyuk.development@gmail.com>
2025-02-11 22:43:20 +0900

    feat: HabitTracker 페이지에 추가

--- src/pages/StudyDetailPage.jsx
+++ src/pages/StudyDetailPage.jsx
@@ -9,6 +9,7 @@ import { getStudy } from "@/api/studyApi";
 import { EarnedPointsBoxMd } from "@/common/EarnedPointsBox";
 import ErrorMessage from "@/common/MessageBox";
 import ShareModal from "@/components/ShareModal";
+import HabitTracker from "@/components/HabitTracker";
 
 function StudyDetailPage() {
   const { studyId } = useParams();
@@ -45,7 +46,7 @@ function StudyDetailPage() {
   };
 
   return (
-    <div className="w-full h-screen bg-[#F6F4EF]">
+    <div className="w-full h-full bg-[#F6F4EF]">
       <Header />
       {!isLoading && (
         <div className="grid place-items-center mt-14">
@@ -114,6 +115,9 @@ function StudyDetailPage() {
                 <div className="mt-2">
                   <EarnedPointsBoxMd points={studyData.totalPoints} />
                 </div>
+                <div>
+                <HabitTracker />
+                </div>
               </>
             ) : (
               <div className="text-center text-32pt">
@@ -140,6 +144,7 @@ function StudyDetailPage() {
           description={studyData.description}
         />
       )}
+      
     </div>
   );
 }
