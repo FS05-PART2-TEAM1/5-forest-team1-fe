14b4aba628e7846716fc56a0797bc675d3e011d3
Ripponcheese <hayooncode@gmail.com>
2025-02-11 09:08:00 +0900

    refactor: Approutrer 수정 및 불필요한 라우터 제거

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StudyDetailPage from "./pages/StudyDetailPage";
 import StudyCreatePage from "./pages/StudyCreatePage";
-import FocusPage from "./pages/FocusPage";
-import HabitPage from "./pages/HabitPage";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/home";
 
@@ -11,12 +9,10 @@ function AppRouter() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/study">
+        <Route path="/studies">
           <Route path="create" element={<StudyCreatePage />} />
           <Route path=":studyId" element={<StudyDetailPage />} />
         </Route>
-        <Route path="/focus" element={<FocusPage />} />
-        <Route path="/habit" element={<HabitPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
