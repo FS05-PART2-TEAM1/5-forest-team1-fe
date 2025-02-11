b1a44853b6b25270e08f78683deeab811a8ad40d
Ripponcheese <hayooncode@gmail.com>
2025-02-11 17:09:53 +0900

    ui: footer

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Footer } from "@/common/layout/Footer";
 import StudyDetailPage from "./pages/StudyDetailPage";
 import StudyCreatePage from "./pages/StudyCreatePage";
 import FocusPage from "./pages/FocusPage";
@@ -9,16 +10,21 @@ import Home from "./pages/home";
 function AppRouter() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/study">
-          <Route path="create" element={<StudyCreatePage />} />
-          <Route path=":studyId" element={<StudyDetailPage />} />
-        </Route>
-        <Route path="/focus" element={<FocusPage />} />
-        <Route path="/habit" element={<HabitPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div className="min-h-screen flex flex-col">
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/study">
+              <Route path="create" element={<StudyCreatePage />} />
+              <Route path=":studyId" element={<StudyDetailPage />} />
+            </Route>
+            <Route path="/focus" element={<FocusPage />} />
+            <Route path="/habit" element={<HabitPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
