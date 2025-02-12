fd2f50af96c87d1b041ffc4bcbd7fc2278acba3a
hyuk-dev <hyuk.development@gmail.com>
2025-02-12 17:51:19 +0900

    fix: 모달 에러 메시지

--- src/common/modal/PasswordModal.jsx
+++ src/common/modal/PasswordModal.jsx
@@ -15,7 +15,6 @@ const PasswordModal = ({
   studyData,
 }) => {
   if (!isOpen) return null;
-
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [password, setPassword] = useState("");
   const [link, setLink] = useState(``);
@@ -42,27 +41,32 @@ const PasswordModal = ({
 
   // 패스워드 검증 기능
   const onVerify = async () => {
-    const verifyData = await verifyPassword(studyId, password);
-    console.log(verifyData);
-    if (verifyData.message) {
-      //검증 완료 시
-      setMessage("");
-      if (buttonText !== "스터디 삭제하기") {
-        navigate(link, { state: { studyData, password } });
-        /*
-          이동한 페이지에서 사용할 때
-          const location = useLocation();
-          console.log(location.state);
-        */
+    try{
+      const verifyData = await verifyPassword(studyId, password);
+      console.log(verifyData);
+      if (verifyData.message) {
+        //검증 완료 시
+        setMessage("");
+        if (buttonText !== "스터디 삭제하기") {
+          navigate(link, { state: { studyData, password } });
+          /*
+            이동한 페이지에서 사용할 때
+            const location = useLocation();
+            console.log(location.state);
+          */
+        }
+        else{
+          deleteStudy(studyId);
+          navigate(link);
+        }
       }
-      else{
-        deleteStudy(studyId);
-        navigate(link);
-      }
-    } else {
+    } catch (err) {
+
       //검증 실패 시
       setMessage("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
     }
+
+    
   };
 
   const onPasswordChange = (e) => {
