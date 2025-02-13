687317429b6650df38900d5e23ae0951a1e6d643
Ripponcheese <hayooncode@gmail.com>
2025-02-13 16:03:09 +0900

    feat:loading page

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,5 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Footer } from "@/common/layout/Footer";
+import Loading from "@/components/Loading";
+import { useState, useEffect } from "react";
 import StudyDetailPage from "./pages/StudyDetailPage";
 import StudyCreatePage from "./pages/StudyCreatePage";
 import StudyModifyPage from "./pages/StudyModifyPage";
@@ -10,27 +12,59 @@ import Home from "./pages/home";
 import TestPage from "./pages/TestPage";
 import LandingPage from "./pages/LandingPage.jsx";
 
+// 페이지 전환을 감지하는 컴포넌트
+const PageTransition = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [showContent, setShowContent] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    const loadSequentially = async () => {
+      setIsLoading(true);
+      setShowContent(false);
+
+      // 로딩 화면을 먼저 보여줌
+      await new Promise((resolve) => setTimeout(resolve, 1600));
+
+      setIsLoading(false);
+      // 로딩이 끝난 후 컨텐츠를 보여줌 (스켈레톤 등의 자체 페이지 로딩 기능 시연을 위해 의도적으로 분리함)
+      setShowContent(true);
+    };
+
+    loadSequentially();
+  }, [location.pathname]);
+
+  return (
+    <>
+      {isLoading && <Loading />}
+      {showContent && children}
+    </>
+  );
+};
+
 function AppRouter() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/study">
-              <Route path="create" element={<StudyCreatePage />} />
-              <Route path=":studyId" element={<StudyDetailPage />} />
-              <Route path="test" element={<TestPage />} />
-            </Route>
-            <Route path="/focus" element={<FocusPage />} />
-            <Route path="/habit" element={<HabitPage />} />
-            <Route path="/modify" element={<StudyModifyPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+      <PageTransition>
+        <div className="min-h-screen flex flex-col">
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/study">
+                <Route path="create" element={<StudyCreatePage />} />
+                <Route path=":studyId" element={<StudyDetailPage />} />
+                <Route path="test" element={<TestPage />} />
+              </Route>
+              <Route path="/focus" element={<FocusPage />} />
+              <Route path="/habit" element={<HabitPage />} />
+              <Route path="/modify" element={<StudyModifyPage />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </PageTransition>
     </BrowserRouter>
   );
 }
