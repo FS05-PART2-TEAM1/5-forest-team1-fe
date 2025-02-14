421b97994e8a5c591d22bac117aa1a5f7fd3bc8b
hyuk-dev <hyuk.development@gmail.com>
2025-02-14 17:42:17 +0900

    feat: 스켈레톤 UI 이모지에도 적용

--- src/components/EmojiForm.jsx
+++ src/components/EmojiForm.jsx
@@ -11,12 +11,16 @@ function EmojiForm({ studyId }) {
   const [isAddMod, setIsAddMod] = useState(false);
   const [isShowAll, setIsShowAll] = useState(false);
   const [isChanged, setIsChanged] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchReactions = async () => {
       const data = await getReactions(studyId);
       setEmojis(data.reactionList);
       setIsChanged(false);
+      setTimeout(()=> {
+        setIsLoading(false)
+      },600)
     };
     if (isChanged) fetchReactions();
   }, [isChanged]);
@@ -25,11 +29,12 @@ function EmojiForm({ studyId }) {
     setIsShowAll(!isShowAll);
   };
 
-  const onEmojiTagClick = async (emoji) => {
+  const onEmojiTagClick = useDebounceCallback(async (emoji) => {
+    setIsLoading(true);
     const findEmoji = emojis.find((element) => element.emoji === emoji);
     await patchReaction(studyId, findEmoji.id, { counts: 1 });
     setIsChanged(true);
-  }
+  }, 200);
 
   const onEmojiClick = useDebounceCallback(async (emojiData) => {
     const isEmoji = emojis.find((element) => emojiData.emoji === element.emoji);
@@ -55,26 +60,37 @@ function EmojiForm({ studyId }) {
 
   return (
     <div className="flex gap-4">
-      <div className="flex gap-1">
-        {emojis.map((element, index) => {
-          if (index < 3)
-            return (
-              <div
-                className="cursor-pointer"
-                key={index}
-                onClick={() => onEmojiTagClick(element.emoji)}
-              >
-                <EmojiTag
-                  emoji={element.emoji}
-                  count={element.counts}
-                  size={"base"}
-                />
-              </div>
-            );
-        })}
-      </div>
+      {!isLoading ? (
+        <div className="flex gap-1">
+          {emojis.map((element, index) => {
+            if (index < 3)
+              return (
+                <div
+                  className="cursor-pointer"
+                  key={index}
+                  onClick={() => onEmojiTagClick(element.emoji)}
+                >
+                  <EmojiTag
+                    emoji={element.emoji}
+                    count={element.counts}
+                    size={"base"}
+                  />
+                </div>
+              );
+          })}
+        </div>
+      ) : (
+        <div className="flex gap-1 animate-pulse bg-gradient-to-r from-red-400 via-yellow-400 to-blue-400 bg-[length:200%_100%] bg-clip-text text-transparent">
+          <div className="flex items-center gap-1 px-2 py-1 bg-f-black bg-opacity-40 rounded-full h-[31px] w-[56px]"></div>
+          <div className="flex items-center gap-1 px-2 py-1 bg-f-black bg-opacity-40 rounded-full h-[31px] w-[56px]"></div>
+          <div className="flex items-center gap-1 px-2 py-1 bg-f-black bg-opacity-40 rounded-full h-[31px] w-[56px]"></div>
+          <div className="flex items-center gap-1 px-2 py-1 bg-f-black bg-opacity-40 rounded-full h-[31px] w-[45px] ml-4"></div>
+          <div className="flex items-center gap-1 px-2 py-1 bg-f-black bg-opacity-40 rounded-full h-[31px] w-[65px] ml-4"></div>
+        </div>
+      )}
+
       <div className="flex">
-        {emojis.length > 3 && (
+        {(emojis.length > 3 && !isLoading) && (
           <div className="lg:block hidden">
             <div
               className="cursor-pointer flex gap-1 p-2 h-8 rounded-[50px] items-center text-[14px] text-white bg-black opacity-20"
@@ -85,10 +101,8 @@ function EmojiForm({ studyId }) {
             </div>
           </div>
         )}
-        {isShowAll && (
-          <div
-            className="lg:visible invisible pl-5 -translate-x-52 border p-4 gap-1 mt-12 absolute bg-white grid grid-cols-4 place-items-center rounded-[20px] "
-          >
+        {(isShowAll&&!isLoading) && (
+          <div className="lg:visible invisible pl-5 -translate-x-52 border p-4 gap-1 mt-12 absolute bg-white grid grid-cols-4 place-items-center rounded-[20px] ">
             {emojis.map((element, index) => {
               return (
                 <div
@@ -107,24 +121,26 @@ function EmojiForm({ studyId }) {
           </div>
         )}
       </div>
-      {emojis &&
-      <>
-      <div className={emojis.length===0 && "relative right-8"}>
-        <div
-          className="border flex lg:w-[70px] md:w-[65px] h-8 text-16pt gap-2 items-center lg:p-1 p-1 rounded-[50px] cursor-pointer"
-          onClick={() => setIsAddMod(!isAddMod)}
-        >
-          <img src={emojiCreateImg} />
-          <div className="md:text-sm lg:text-lg">추가</div>
-        </div>
-        {isAddMod && (
-          <div className="absolute mt-3 md:translate-x-0 -translate-x-48 z-20">
-            <EmojiPicker onEmojiClick={onEmojiClick} />
-          </div>
+      <div>
+        {!isLoading && (
+          <>
+            <div className={emojis.length === 0 && "relative right-8"}>
+              <div
+                className="border flex lg:w-[70px] md:w-[65px] h-8 text-16pt gap-2 items-center lg:p-1 p-1 rounded-[50px] cursor-pointer"
+                onClick={() => setIsAddMod(!isAddMod)}
+              >
+                <img src={emojiCreateImg} />
+                <div className="md:text-sm lg:text-lg">추가</div>
+              </div>
+              {isAddMod && (
+                <div className="absolute mt-3 md:translate-x-0 -translate-x-48 z-20">
+                  <EmojiPicker onEmojiClick={onEmojiClick} />
+                </div>
+              )}
+            </div>
+          </>
         )}
-        </div>
-        </>
-      }
+      </div>
     </div>
   );
 }
--- src/pages/StudyDetailPage.jsx
+++ src/pages/StudyDetailPage.jsx
@@ -21,7 +21,7 @@ function StudyDetailPage() {
   useEffect(() => {
     const timeOut = setTimeout(() => {
       setIsLoading(false);
-    },500);
+    }, 1000);
     return () => clearTimeout(timeOut);
   }, []);
 
@@ -55,93 +55,90 @@ function StudyDetailPage() {
       <Header />
       <div className="min-h-screen place-items-center py-10 md:py-20 ">
         <div className="bg-white w-[95%]  min-w-[380px] mx-auto rounded-[20px] p-6 md:p-10 shadow-lg md:max-w-[1248px]">
-          {(!isLoading) ? (
-              studyData?.title?(
-                <>
-<div className="flex md:flex-row flex-col-reverse justify-between gap-3">
-                <EmojiForm studyId={studyId} />
-                <div className="flex gap-4 md:justify-start justify-end mt-4">
-                  <div
-                    className="text-[#578246] text-16pt cursor-pointer"
-                    onClick={enableShare}
-                  >
-                    공유하기
-                  </div>
-                  <div className="text-[#818181] text-16pt">|</div>
-                  <div
-                    className="text-[#578246] text-16pt cursor-pointer"
-                    data-name="수정하러 가기"
-                    onClick={(e) => enableModal(e)}
-                  >
-                    수정하기
-                  </div>
-                  <div className="text-[#818181] text-16pt">|</div>
-                  <div
-                    className="text-[#818181] text-16pt cursor-pointer"
-                    onClick={(e) => enableModal(e)}
-                    data-name="스터디 삭제하기"
-                  >
-                    스터디 삭제하기
-                  </div>
-                </div>
+          <div className="flex md:flex-row flex-col-reverse justify-between gap-3">
+            {
+              studyData &&
+              <>
+<EmojiForm studyId={studyId} />
+            <div className="flex gap-4 md:justify-start justify-end mt-4">
+              <div
+                className="text-[#578246] text-16pt cursor-pointer"
+                onClick={enableShare}
+              >
+                공유하기
               </div>
-              <div className="flex md:flex-row flex-col justify-between mt-8 gap-4">
-                <div className="md:text-32pt text-24pt font-extrabold">
-                  {studyData.nickname + "의 " + studyData.title}
-                </div>
-                <div className="flex gap-4">
-                  <div
-                    className="flex items-center justify-center text-[#818181] border border-[#DDDDDD] md:w-[144px] w-[120px] rounded-2xl text-16pt md:h-[48px] h-[40px] cursor-pointer"
-                    onClick={(e) => enableModal(e)}
-                    data-name="오늘의 습관으로 가기"
-                  >
-                    오늘의 습관 <img src={arrowImg} className="ml-3" />
-                  </div>
-                  <div
-                    className="flex items-center justify-center text-[#818181] border border-[#DDDDDD] md:w-[144px] w-[120px] rounded-2xl text-16pt cursor-pointer md:h-[48px] h-[40px] "
-                    onClick={(e) => enableModal(e)}
-                    data-name={"오늘의 집중으로 가기"}
-                  >
-                    오늘의 집중 <img src={arrowImg} className="ml-3" />
-                  </div>
-                </div>
+              <div className="text-[#818181] text-16pt">|</div>
+              <div
+                className="text-[#578246] text-16pt cursor-pointer"
+                data-name="수정하러 가기"
+                onClick={(e) => enableModal(e)}
+              >
+                수정하기
               </div>
-              <div className="mt-4 text-wrap">
-                <div className="text-[#818181] md:text-18pt text-16pt">
-                  소개
-                </div>
-                <div className="break-all mt-2 md:text-18pt text-16pt">
-                  {studyData.description}
-                </div>
+              <div className="text-[#818181] text-16pt">|</div>
+              <div
+                className="text-[#818181] text-16pt cursor-pointer"
+                onClick={(e) => enableModal(e)}
+                data-name="스터디 삭제하기"
+              >
+                스터디 삭제하기
               </div>
-              <div className="mt-6 md:text-18pt text-16pt text-[#818181]">
-                현재까지 획득한 포인트
-              </div>
-              <div className="mt-2">
-                <EarnedPointsBoxMd points={studyData.totalPoints} />
-              </div>
-              <div className="mt-4 flex-grow">
-                <HabitTracker studyId={studyId} />
               </div>
               </>
-              ):(
-                <>
-                                  <div className="text-24pt text-center ">
-                    존재하지 않는 스터디입니다.
+            }
+          </div>
+          {!isLoading ? (
+            studyData?.title ? (
+              <>
+                <div className="flex md:flex-row flex-col justify-between mt-8 gap-4">
+                  <div className="md:text-32pt text-24pt font-extrabold">
+                    {studyData.nickname + "의 " + studyData.title}
                   </div>
-                </>
-              )
+                  <div className="flex gap-4">
+                    <div
+                      className="flex items-center justify-center text-[#818181] border border-[#DDDDDD] md:w-[144px] w-[120px] rounded-2xl text-16pt md:h-[48px] h-[40px] cursor-pointer"
+                      onClick={(e) => enableModal(e)}
+                      data-name="오늘의 습관으로 가기"
+                    >
+                      오늘의 습관 <img src={arrowImg} className="ml-3" />
+                    </div>
+                    <div
+                      className="flex items-center justify-center text-[#818181] border border-[#DDDDDD] md:w-[144px] w-[120px] rounded-2xl text-16pt cursor-pointer md:h-[48px] h-[40px] "
+                      onClick={(e) => enableModal(e)}
+                      data-name={"오늘의 집중으로 가기"}
+                    >
+                      오늘의 집중 <img src={arrowImg} className="ml-3" />
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-4 text-wrap">
+                  <div className="text-[#818181] md:text-18pt text-16pt">
+                    소개
+                  </div>
+                  <div className="break-all mt-2 md:text-18pt text-16pt">
+                    {studyData.description}
+                  </div>
+                </div>
+                <div className="mt-6 md:text-18pt text-16pt text-[#818181]">
+                  현재까지 획득한 포인트
+                </div>
+                <div className="mt-2">
+                  <EarnedPointsBoxMd points={studyData.totalPoints} />
+                </div>
+                <div className="mt-4 flex-grow">
+                  <HabitTracker studyId={studyId} />
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="text-24pt text-center ">
+                  존재하지 않는 스터디입니다.
+                </div>
+              </>
+            )
           ) : (
             <div className="animate-pulse">
-              {/* 상단 버튼 영역 */}
-              <div className="flex md:flex-row flex-col-reverse justify-between gap-3">
-                <div className="h-10 bg-gray-300 rounded w-[120px] md:w-[144px]"></div>
-                <div className="flex gap-4 md:justify-start justify-end mt-4">
-                  <div className="h-6 bg-gray-300 rounded w-[90px]"></div>
-                  <div className="h-6 bg-gray-300 rounded w-[90px]"></div>
-                  <div className="h-6 bg-gray-300 rounded w-[90px]"></div>
-                </div>
-              </div>
+
 
               {/* 제목 */}
               <div className="flex md:flex-row flex-col justify-between mt-8 gap-4">
